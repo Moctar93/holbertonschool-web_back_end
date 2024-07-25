a0f5bc9a6dc9a2e31166fdf1839b13e3ab77df72
root <root@Moctinet>
2024-07-25 15:48:25 +0200

    function handleResponseFromAPI(promise)

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,19 @@
+// 2-then.js
+
+function handleResponseFromAPI(promise) {
+    return promise
+        .then((response) => {
+            console.log('Got a response from the API');
+            return {
+                status: 200,
+                body: 'success',
+            };
+        })
+        .catch((error) => {
+            console.log('Got a response from the API');
+            return new Error();
+        });
+}
+
+export default handleResponseFromAPI;
+
