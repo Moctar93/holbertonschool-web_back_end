08c330701753be9704432b040d9e349e1240f499
root <root@Moctinet>
2024-09-20 22:27:49 +0200

    Using Process stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,11 @@
-console.log("welcome to holberton school, what is your name?")
+console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('data', (input) => {
-	const name = input.toString().trim();
+process.stdin.on('data', (data) => {
+  const input = data.toString().trim();
+  console.log(`Your name is: ${input}`);
+  process.exit();
+});
 
-	console.log(`Your name is: ${name}`);
-
-	console.log('This important software is now closing');
-	
-	process.exit();
-
+process.on('exit', () => {
+  console.log('This important software is now closing');
 });
